a0bd651c3d9a3815412c909fae3aafb2f00e2053
alipbekovdauren <daurenh09@gmail.com>
2019-06-18 15:46:30 +0600

    Updates

---
+++ precache-manifest.6954a4cce4d20c19114609b2aac3e21d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3aa9b2069677f1a361c151c4ba762a24",
+    "url": "/contactManagerWithRedux/index.html"
+  },
+  {
+    "revision": "0c70f23b3f1af65f094f",
+    "url": "/contactManagerWithRedux/static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "3f8aaa84a1e93f2582ca",
+    "url": "/contactManagerWithRedux/static/js/2.77b87d00.chunk.js"
+  },
+  {
+    "revision": "0c70f23b3f1af65f094f",
+    "url": "/contactManagerWithRedux/static/js/main.5f416e4f.chunk.js"
+  },
+  {
+    "revision": "6e3737cce49a4589ba46",
+    "url": "/contactManagerWithRedux/static/js/runtime~main.97541ea1.js"
+  }
+]);
